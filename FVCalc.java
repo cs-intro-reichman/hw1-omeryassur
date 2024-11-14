20ab72276988d24486a1cfa2622d6df3391f2314
Omer Yassur <omeryassur@Omers-MacBook-Air.local>
2024-11-14 20:18:40 +0200

    Completed Assignment

--- FVCalc.java
+++ FVCalc.java
@@ -1,15 +1,14 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
-	public static void main(String[] args){
-		// Replace this comment with your code
-		String currentValue = args[0];
-		String rate = args[1];
-		String years = args[2];
-		int currentValueInt = Integer.parseInt(currentValue);
-		double rateDouble = Double.parseDouble(rate);
-		int yearsInt = Integer.parseInt(years);
-
-		double futureValue = (currentValueInt*Math.pow(1+rateDouble/100,yearsInt));
-		System.out.println("After "+ years+ " years, $"+ currentValue+ " saved at "+ rateDouble+ "% will yield $"+ (int)futureValue);
-	}
+    public static void main(String[] args)
+    {
+        double currentValue =Double.parseDouble(args[0]);
+        double rate = Double.parseDouble(args[1]);
+        int n = Integer.parseInt(args[2]);
+        double futureValue = currentValue * Math.pow(1+(rate/100),(double)n);
+        
+        System.out.println("After " + n + " years, a $"
+         +(int)currentValue+ " saved at " + rate + "% will yield $" + 
+         (int)futureValue);
+    }
 }
--- NumWords.java
+++ NumWords.java
@@ -1,20 +1,14 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
-	public static void main(String args[]) {
-	    String num = args[0];
-
-		char ones = num.charAt(num.length()-1);
-
-		char tens;
-		if (num.length() >= 2){
-			tens = num.charAt(num.length()-2);
-		} else {
-			tens = '0';
-		}
-
-		int numInt = Integer.parseInt(num);
-		int hundredsInt = numInt/100;
-
-		System.out.println(hundredsInt+ " hundreds, "+ tens+ " tens, and "+ ones+ " ones");
-	}
+    public static void main(String args[]) 
+    {
+       int nW = Integer.parseInt(args[0]);
+       int hundreds = (nW/100);
+       int tens = (nW/10)%10;
+       int ones = nW%10;
+       System.out.println(hundreds  + " hundreds, " +tens + " tens, and " +ones + " ones.");
+     
+       
+    }
 }
+
